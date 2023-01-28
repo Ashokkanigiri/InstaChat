9ddaf860d167d869b9a15917473dd61ba366d53e
Ashokkanigiri <ashkanigiri@gmail.com>
2023-01-28 12:24:11 +0530

    Home Fragment clean up

--- app/src/main/java/com/example/instachat/feature/home/HomeFragment.kt
+++ app/src/main/java/com/example/instachat/feature/home/HomeFragment.kt
@@ -1,26 +1,22 @@
 package com.example.instachat.feature.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.example.instachat.R
 import com.example.instachat.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
 
-    private val viewModel : HomeViewModel by viewModels()
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,18 +28,25 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initFragment()
+        loadDataFromViewModel()
+        handleSwipeLayout()
+    }
 
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
-        viewModel.getAllDataFromFirebase()
-
+    private fun handleSwipeLayout() {
         binding.swipeLayout.setOnRefreshListener {
             viewModel.getAllDataFromFirebase()
             binding.swipeLayout.isRefreshing = false
         }
-
-
     }
 
+    private fun loadDataFromViewModel() {
+        viewModel.getAllDataFromFirebase()
+    }
+
+    private fun initFragment() {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
 
 }
